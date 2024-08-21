bd84cd82216894fdc035501a0c785e05d52b8afa
Paweł <81538700+PawiX25@users.noreply.github.com>
2024-08-21 23:01:47 +0000

    Add character count functionality to word count

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function countWords() {
     var inputText = document.getElementById("input").value;
     var wordCount = inputText.trim().split(/\s+/).length;
-    document.getElementById("result").innerHTML = "Word count: " + wordCount;
+    var charCount = inputText.length;
+    document.getElementById("result").innerHTML = "Word count: " + wordCount + "<br>Character count: " + charCount;
 }
